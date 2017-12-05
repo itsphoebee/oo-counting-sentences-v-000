b7cc5ad8aed5992faa8e12d6ca0f5260030d5dd5
Phoebe Lau <phoebelau7@gmail.com>
2017-12-05 22:34:43 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,7 +3,7 @@ require 'pry'
 class String
 
   def sentence?
-    self.end_with?(".")
+    end_with?(".")
   end
 
   def question?
@@ -15,5 +15,6 @@ class String
   end
 
   def count_sentences
+    self.split
   end
 end
